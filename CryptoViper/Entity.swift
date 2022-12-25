601df7116ea0e7a9091af4a8454a8c2cf3d2eba6
Furkan Keskin <ksknmhmt3131@gmail.com>
2022-12-25 20:47:51 +0300

    Initial Commit

---
+++ CryptoViper/Entity.swift
@@ -0,0 +1,13 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Furkan on 25.12.2022.
+//
+
+import Foundation
+
+struct Crypto : Decodable{
+    let currency : String
+    let price : String
+}
